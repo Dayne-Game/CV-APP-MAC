b1b9053de713ee577a50dc808fda204c175c3091
Dayne-Game <daynegame024@gmail.com>
2019-09-08 13:32:12 +1200

    Inital Commit

---
+++ MyActualAssignment/MyActualAssignment/SkillSetPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MyActualAssignment
+{
+    public class SkillSetPage : ContentPage
+    {
+        public SkillSetPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ MyActualAssignment/MyActualAssignment/SkillSetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MyActualAssignment
+{
+    public partial class SkillSetPage : ContentPage
+    {
+        public SkillSetPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BackToMainPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
